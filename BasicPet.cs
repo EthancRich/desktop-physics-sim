68627a9284674c4792aff7caadd610551f9d62dc
Ethan Rich <ethanc.rich@gmail.com>
2026-02-26 20:52:56 -0700

    Draggable Pet

--- BasicPet.cs
+++ BasicPet.cs
@@ -1,40 +1,111 @@
 namespace DesktopPhysicsSim;
 
+using System.Collections.Generic;
+using System.Diagnostics;
 using Godot;
 
 public partial class BasicPet : Node2D
 {
 	private bool _dragging = false;
-	private int _clickRadius = 64;
+	private int _spriteWidth;
+	private int _spriteHeight;
+
+	private const int DELTA_SCALAR = 1;
+	private Vector2 _gravityAccel = new Vector2( 0, 1000 );
+	private Vector2 _velocity = new Vector2( 0, 0 ); // Pixels per second
+
+	private struct MouseSample
+	{
+		public Vector2 Position;
+		public double Time;
+	}
+	private List<MouseSample> _samples = [];
+
+	public override void _Ready()
+	{
+		_spriteWidth = GetNode<Sprite2D>( "Sprite2D" ).Texture.GetWidth();
+		_spriteHeight = GetNode<Sprite2D>( "Sprite2D" ).Texture.GetHeight();
+	}
+
+	public override void _Process( double delta )
+	{
+		if ( !_dragging )
+		{
+			Vector2 deltaVec2 = new Vector2( (float)delta, (float)delta );
+
+			Vector2 pos = (Vector2)GetWindow().Position;
+			pos += _velocity * deltaVec2 * DELTA_SCALAR;
+			GetWindow().Position = (Vector2I)pos;
+		}
+	}
+
+
+	public override void _PhysicsProcess( double delta )
+	{
+		if ( !_dragging )
+		{
+			Vector2 deltaVec2 = new Vector2( (float)delta, (float)delta );
+			_velocity += _gravityAccel * deltaVec2 * DELTA_SCALAR;
+		}
+	}
+
 
 	public override void _Input( InputEvent @event )
 	{
-
 		if ( @event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left )
 		{
-			GD.Print( mouseEvent.Position );
-			GD.Print( Position );
-			if ( (mouseEvent.Position - Position).Length() < _clickRadius )
+			// Start dragging if the click is on the sprite.
+			if ( !_dragging && mouseEvent.Pressed )
 			{
-				// Start dragging if the click is on the sprite.
-				if ( !_dragging && mouseEvent.Pressed )
-				{
-					_dragging = true;
-				}
+				_dragging = true;
 			}
 			// Stop dragging if the button is released.
 			if ( _dragging && !mouseEvent.Pressed )
 			{
 				_dragging = false;
+
+				if ( _samples.Count >= 2 )
+				{
+					var last = _samples[^1];
+					var first = _samples[0];
+
+					Vector2 dp = last.Position - first.Position;
+					double dt = last.Time - first.Time;
+
+					if ( dt > 0 )
+					{
+						_velocity = dp / (float)dt;
+					}
+					GD.Print( _velocity );
+				}
+
+				_samples.Clear();
 			}
 		}
 		else
 		{
 			if ( @event is InputEventMouseMotion motionEvent && _dragging )
 			{
+				MouseSample sample = new MouseSample
+				{
+					Position = DisplayServer.MouseGetPosition(),
+					Time = Time.GetTicksMsec() / 1000.0
+				};
+				_samples.Add( sample );
 
-				// While dragging, move the sprite with the mouse.
-				Position = motionEvent.Position;
+				if ( _samples.Count > 3 )
+				{
+					_samples.RemoveAt( 0 );
+				}
+
+
+				// // While dragging, move the sprite with the mouse.
+				// var prevPos = GetWindow().Position;
+				var newPos = DisplayServer.MouseGetPosition();
+				GetWindow().Position = newPos;
+
+				// _velocity = (newPos - prevPos);
+
 			}
 		}
 	}
--- Main.cs
+++ Main.cs
@@ -5,18 +5,40 @@ using Godot;
 public partial class Main : Node2D
 {
 
+	[Export]
+	public PackedScene PetScene { get; set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Sprite2D icon = GetNode<Sprite2D>( "Icon" );
-		float width = icon.Texture.GetWidth() * icon.Scale.X;
-		float height = icon.Texture.GetHeight() * icon.Scale.Y;
-
+		// Create the first window -- home
 		var window = GetWindow();
 		window.AlwaysOnTop = true;
-		window.Transparent = true;
-		window.Borderless = true;
-		window.Size = new Vector2I( (int)width, (int)height );
+		window.Transparent = false;
+		window.Borderless = false;
+		window.Size = new Vector2I( 500, 300 );
 
+		// Create a basic pet
+		CallDeferred( nameof( CreatePetWindow ), window );
+	}
+
+	private void CreatePetWindow( Window window )
+	{
+		// I think the best thing to do here would be to decouple the sprite reference from the child of the pet node
+		BasicPet pet = PetScene.Instantiate<BasicPet>();
+		Sprite2D sprite = pet.GetNodeOrNull<Sprite2D>( "Sprite2D" );
+		pet.Position = new Vector2I( sprite.Texture.GetWidth() / 2, sprite.Texture.GetHeight() / 2 );
+
+		var newWindow = new Window();
+		newWindow.Transparent = true;
+		newWindow.Borderless = true;
+		newWindow.AlwaysOnTop = true;
+		newWindow.Position = new Vector2I( window.Position.X - 50, window.Position.Y - 50 );
+		newWindow.Size = new Vector2I( sprite.Texture.GetWidth(), sprite.Texture.GetHeight() );
+
+		newWindow.AddChild( pet );
+		GetTree().Root.AddChild( newWindow );
+
+		newWindow.Show();
 	}
 }
